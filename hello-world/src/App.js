fe6688a5f74f50ec66ab6456e5a6bbc621d38167
HoangVu <hoangvu1991uit@gmail.com>
2020-03-04 11:21:54 +0700

    reformat

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -3,24 +3,24 @@ import React from 'react';
 export default class App extends React.Component {
 
   constructor(props){
-    super(props);  
-    
+    super(props);
+
     this.state = {
     query: '',
     };
-    
+
     this.onChange=this.onChange.bind(this);
   }
-  
+
   onChange(event) {
     this.setState({ query: event.target.value });
   }
-  
+
   render() {
   const users = [
       { name: 'Robin' },
       { name: 'Markus' },
-    
+
     ];
     return (
       <div>
